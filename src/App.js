0cc16384878ca890429793dea8f3a1c35f8a64bb
Bill Rogers <bill@Bills-MacBook-Air.local>
2022-07-10 21:39:23 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -6,32 +6,34 @@ import CreateData from './CreateData';
 const ref = firebase.firestore().collection('developers');
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(true);
+	const [data, setData] = useState([]);
+	const [loader, setLoader] = useState(false);
 
-  function GetData() {
-    ref.onSnapshot((querySnapshot) => {
-      const items = [];
-      querySnapshot.forEach((doc) => {
-        items.push(doc.data());
-      });
-      setData(items);
-      setLoader(false);
-    });
-  }
-  useEffect(() => {
-    GetData();
-    console.log(data);
-  }, []);
-  return (
-    <div className="App">
-      <h1 style={{ padding: '30px', textAlign: 'center', fontSize: '25px' }}>
-        React Firestore CRUD App
-      </h1>
-      {loader === false && data.map((dev) => <DataBlock dev={dev} />)}
-      <CreateData />
-    </div>
-  );
+	function GetData() {
+		ref.onSnapshot((querySnapshot) => {
+			const items = [];
+			querySnapshot.forEach((doc) => {
+				items.push(doc.data());
+			});
+			setData(items);
+			setLoader(false);
+		});
+	}
+	useEffect(() => {
+		GetData();
+		console.log(data);
+	}, []);
+	return (
+		<div className="App">
+			<h1 style={{ padding: '30px', textAlign: 'center', fontSize: '25px' }}>
+				React Firestore CRUD App
+			</h1>
+			{data.map((dev) => (
+				<DataBlock dev={dev} />
+			))}
+			<CreateData />
+		</div>
+	);
 }
 
 export default App;
